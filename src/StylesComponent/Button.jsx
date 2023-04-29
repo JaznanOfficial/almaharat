c402a50ed5200ad09a364d2ab320d389a58e9f9e
Nijumdip <nijumdip019911@gmail.com>
2023-04-29 20:03:21 +0600

    all btns on homepage done

---
+++ src/StylesComponent/Button.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+    border-radius: 8px;
+    border: none;
+    padding: 1rem 2.188rem;
+    margin: 1rem;
+    font-size: 1.125rem;
+    font-weight: 500;
+    font-family: inherit;
+    background-color: #309255;
+    color: white;
+    cursor: pointer;
+    text-transform: capitalize;
+    transition: all 0.3s ease;
+
+    &:hover {
+        background-color: #68686d;
+    }
+    &:focus {
+        color: #1d2733;
+    }
+`;
---
+++ src/components/Homepage/HeroSection.jsx
@@ -0,0 +1,13 @@
+import { Button } from "../../StylesComponent/Button"
+
+const HeroSection = () => {
+  return (
+      <div>
+          
+          <Button>start a course</Button>
+      
+      </div>
+  )
+}
+
+export default HeroSection
