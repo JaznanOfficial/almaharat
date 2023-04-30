5d57abf9f7d96fa66badb7d2328e70ab8447509d
Nijumdip <nijumdip019911@gmail.com>
2023-04-30 12:46:59 +0600

    first commit

--- src/StylesComponent/Button.jsx
+++ src/StylesComponent/Button.jsx
@@ -2,22 +2,26 @@ import styled from "styled-components";
 
 export const Button = styled.button`
     border-radius: 8px;
-    border: none;
+    border: 1px solid #309255;
     padding: 1rem 2.188rem;
     margin: 1rem;
     font-size: 1.125rem;
     font-weight: 500;
     font-family: inherit;
     background-color: #309255;
+    background: linear-gradient(to left, #309255 50%, white 50%) right;
+    background-size: 200% 100%;
     color: white;
     cursor: pointer;
     text-transform: capitalize;
-    transition: all 0.3s ease;
-
+    transition: all 0.5s ease;
     &:hover {
-        background-color: #68686d;
+        background-position: left;
+        transition: all 2s ease;
+        background-color: white;
+        color: #309255;
     }
-    &:focus {
+    /* &:focus {
         color: #1d2733;
-    }
+    } */
 `;
